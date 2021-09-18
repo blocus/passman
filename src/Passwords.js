de63172ca0a3c95319ddae64238a73613ba5e763
Ahmed Meftah <ahmedme@xtech.guru>
2021-09-18 17:57:06 +0100

    Read data from passwords file

--- src/Passwords.js
+++ src/Passwords.js
@@ -1,8 +1,29 @@
+import { useEffect, useState } from 'react'
+
+const fs = window.require('fs')
+
 function Passwords({ path }) {
+  const [data, setData] = useState('')
+
+  const readFile = path => {
+    setData(() => JSON.parse(fs.readFileSync(path, { encoding: 'utf8', flag: 'r' })))
+  }
+
+  useEffect(() => {
+    if (fs.existsSync(path)) {
+      readFile(path)
+    } else {
+      fs.writeFile(path, '[]', () => readFile(path))
+    }
+  }, [path])
+
   return (
-    <code>
-      <pre>{path}</pre>
-    </code>
+    <div className='container my-3'>
+      <h2>{path}</h2>
+      <code>
+        <pre>{JSON.stringify(data, undefined, 2)}</pre>
+      </code>
+    </div>
   )
 }
 
